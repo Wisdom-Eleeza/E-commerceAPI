77c8007fa8476dab71356ea13c9d1bfd242a870f
Wisdom Eleeza <wisdom.eleeza@amalitech.com>
2023-07-06 09:58:23 +0000

    Start Working on E-commerce API

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log("MongoDB Connected Successfully")
+    } catch (error) {
+        console.error("Failed to Connect to MongoDB: ", error)
+    }
+}
+
+module.exports = connectDB

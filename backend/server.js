037c9ddef474598387ca020f1be04c0a8b30da64
Wisdom Eleeza <wisdom.eleeza@amalitech.com>
2023-07-06 11:05:03 +0000

    Create userModel and folders for other models

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT || 3001
+const express = require("express");
+const app = express();
+const connectDB = require("./config/db");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const port = process.env.PORT || 3001;
 
+connectDB(); // connect to database
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+// app.use("/api/users", );
+
+app.get("/api/users/status", (req, res) => {
+  res.send("API is running");
+});
 
 app.listen(port, () => {
-    console.log(`Server listening on http://localhost: ${port}`)
-})
-
-
-
+  console.log(`Server listening on http://localhost: ${port}`);
+});
